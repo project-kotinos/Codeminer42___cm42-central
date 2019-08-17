754eb48f311cfe66e858118dda2151c0bfbc6615
John Ewart <john@johnewart.net>
2019-08-17 11:46:31 -0700

    Fix to shell script

--- yourbased.sh
+++ yourbased.sh
@@ -10,9 +10,11 @@ export LC_ALL=en_US.UTF-8
 export RAILS_ENV=test
 export RACK_ENV=test
 export MERB_ENV=test
+echo "PATH IS $PATH"
 
 apt-get update && apt-get install -y tzdata libpq-dev unzip xvfb libnss3 libgconf-2-4 libxss1 fonts-liberation
 
+gem install bundler
 bundle install
 npm install
 yarn install
@@ -22,7 +24,6 @@ cp .env.sample .env
 
 # Install chrome and chrome driver
 if [ ! -f /usr/bin/google-chrome ]; then
-  CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
   curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
   echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
   apt-get -y update
@@ -30,6 +31,7 @@ if [ ! -f /usr/bin/google-chrome ]; then
 fi
 
 if [ ! -f /opt/bin/chromedriver ]; then
+  CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
   wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
   mkdir -p /opt/bin
   unzip -o /tmp/chromedriver_linux64.zip -d /opt/bin
@@ -41,8 +43,7 @@ if [ ! -f /opt/bin/chromedriver-bin ]; then
   mv /opt/bin/chromedriver /opt/bin/chromedriver-bin
   echo "#!/bin/bash" > /opt/bin/chromedriver
   echo "/opt/bin/chromedriver-bin --whitelisted-ips='' \$*" >> /opt/bin/chromedriver
-  export PATH="$PATH:/opt/bin"
-  echo "PATH IS $PATH"
+  chmod a+x /opt/bin/chromedriver
 fi 
 
 if [ ! -f /usr/bin/google-chrome-binary ]; then 
@@ -51,6 +52,9 @@ if [ ! -f /usr/bin/google-chrome-binary ]; then
   echo "/usr/bin/google-chrome-binary --no-sandbox \$*" >> /usr/bin/google-chrome
   chmod a+x /usr/bin/google-chrome
 fi
+
+export PATH="$PATH:/opt/bin"
+ echo "PATH IS $PATH"
 
 bundle exec rake --trace fulcrum:setup db:setup
 xvfb-run bundle exec rake travis
