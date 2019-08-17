2cf01aacae903eb8b26660a047847986b8f09da7
John Ewart <john@johnewart.net>
2019-08-16 23:42:17 -0700

    Make chromedriver work; also redis was missing

--- yourbased.sh
+++ yourbased.sh
@@ -11,28 +11,47 @@ export RAILS_ENV=test
 export RACK_ENV=test
 export MERB_ENV=test
 
-apt-get update && apt-get install -y tzdata libpq-dev unzip
-#install:
+apt-get update && apt-get install -y tzdata libpq-dev unzip xvfb libnss3 libgconf-2-4 libxss1 fonts-liberation
+
 bundle install
 npm install
 yarn install
 
-
-#before_script:
 cp config/database.yml.example config/database.yml
 cp .env.sample .env
-wget http://chromedriver.storage.googleapis.com/2.24/chromedriver_linux64.zip
-mkdir -p /opt/bin
-unzip chromedriver_linux64.zip
-cp chromedriver /opt/bin/chromedriver
-export PATH="$PATH:/opt/bin"
-echo "PATH IS $PATH"
-chmod +x /opt/bin/chromedriver
 
-# "export DISPLAY=:99.0"
-# "sh -e /etc/init.d/xvfb start"
+# Install chrome and chrome driver
+if [ ! -f /usr/bin/google-chrome ]; then
+  CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+  curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+  apt-get -y update
+  apt-get -y install google-chrome-stable
+fi
+
+if [ ! -f /opt/bin/chromedriver ]; then
+  wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
+  mkdir -p /opt/bin
+  unzip -o /tmp/chromedriver_linux64.zip -d /opt/bin
+  rm /tmp/chromedriver_linux64.zip
+  chmod a+x /opt/bin/chromedriver
+fi 
+
+if [ ! -f /opt/bin/chromedriver-bin ]; then
+  mv /opt/bin/chromedriver /opt/bin/chromedriver-bin
+  echo "#!/bin/bash" > /opt/bin/chromedriver
+  echo "/opt/bin/chromedriver-bin --whitelisted-ips='' \$*" >> /opt/bin/chromedriver
+  export PATH="$PATH:/opt/bin"
+  echo "PATH IS $PATH"
+fi 
+
+if [ ! -f /usr/bin/google-chrome-binary ]; then 
+  mv /usr/bin/google-chrome /usr/bin/google-chrome-binary
+  echo "#!/bin/bash" > /usr/bin/google-chrome
+  echo "/usr/bin/google-chrome-binary --no-sandbox \$*" >> /usr/bin/google-chrome
+  chmod a+x /usr/bin/google-chrome
+fi
+
 bundle exec rake --trace fulcrum:setup db:setup
-
-#script:
-bundle exec rake travis
+xvfb-run bundle exec rake travis
 bundle exec codeclimate-test-reporter
